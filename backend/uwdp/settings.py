6d59f266c1cd670d2d0fb5fc69995d0febb50d76
TDC28 <a06x03g07t01@protonmail.com>
2024-08-20 18:58:24 -0300

    Setup CORS to allow API calls

--- backend/uwdp/settings.py
+++ backend/uwdp/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "uwdp.urls"
@@ -130,3 +131,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True

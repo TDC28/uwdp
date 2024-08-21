d449f031455c6217f6c54f6d798c07e872f3c9db
TDC28 <a06x03g07t01@protonmail.com>
2024-08-20 22:53:32 -0300

    Added unfold and rewrote requirements.txt

--- backend/uwdp/settings.py
+++ backend/uwdp/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # "jazzmin",
+    "unfold",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

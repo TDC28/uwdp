e2f065c90f45743d0aabaa67cb0bd71312e7f4c9
TDC28 <a06x03g07t01@protonmail.com>
2024-08-29 15:04:06 -0300

    Logo added to Nav

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -16,21 +16,32 @@ export default function Nav() {
     <NavigationMenu>
       <NavigationMenuList>
         <NavigationMenuItem>
+          <Link href="/" legacyBehavior passHref>
+            <img src="/logo.png" alt="UWDP Logo" className="h-14 pl-2" />
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
           <Link href="/dashboard" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>Dashboard</NavigationMenuLink>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Dashboard
+            </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <Link href="/auth/login" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>Login</NavigationMenuLink>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Login
+            </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <Link href="/auth/register" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>Register</NavigationMenuLink>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Register
+            </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
-  )
+  );
 }

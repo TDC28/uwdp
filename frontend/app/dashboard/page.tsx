afc30e2174a39ce5bd65b4c1344708d1ec826667
TDC28 <a06x03g07t01@protonmail.com>
2024-09-02 18:05:04 -0400

    Rerouting on login

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { Card } from "@/components/ui/";
+
 export default function DashboardPage() {
-  return <></>;
+  return (
+    <div className="h-screen w-screen sm:bg-gradient-to-b from-green-300 to-indigo-400">
+      Hello
+    </div>
+  );
 }

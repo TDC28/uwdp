1242b5d0e38798d22f658659d84b8e5bd282c0d0
TDC28 <a06x03g07t01@protonmail.com>
2024-09-02 23:30:13 -0400

    Dashboard loads user's terms

--- backend/uwdp/settings.py
+++ backend/uwdp/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOW_CREDENTIALS = True
 
 
 REST_FRAMEWORK = {
--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,11 +1,67 @@
 "use client";
 
-import { Term } from "@/components/Term";
+import { useEffect, useState } from "react";
+
+interface Term {
+  username: string;
+  study_term: string;
+  courses: string[];
+}
 
 export default function DashboardPage() {
+  const [userTerms, setTermsData] = useState<Term[]>([]);
+
+  useEffect(() => {
+    const terms = [];
+    const getTerms = async () => {
+      const response = await fetch("http://localhost:8000/api/terms/", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setTermsData(data);
+      } else {
+        console.log("An error occured fetching terms");
+        // Do stuff
+      }
+    };
+    getTerms();
+  }, []);
+
+  const handleInputChange = (
+    termIndex: number,
+    courseIndex: number,
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const newTermsData = [...userTerms];
+
+    newTermsData[termIndex].courses[courseIndex] = e.target.value;
+    setTermsData(newTermsData);
+  };
+
+  //for (let i = 0; i < inputFields.length; i++) {
+  //  terms.push(inputFields)
+  //}
   return (
     <div className="h-screen w-screen sm:bg-gradient-to-b from-green-300 to-indigo-400">
-      <Term />
+      {userTerms.map((term, termIndex) => (
+        <div key={termIndex}>
+          <h2>Term {termIndex + 1}</h2>
+          {term.courses.map((course, courseIndex) => (
+            <input
+              key={courseIndex}
+              value={course}
+              onChange={(e) => handleInputChange(termIndex, courseIndex, e)}
+            ></input>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }

6afb8475f6a37984bcc456f428cae47a2c4399de
TDC28 <a06x03g07t01@protonmail.com>
2024-09-12 13:02:23 -0400

    Migrated Degree model and removal of Term and TermSerializer

--- backend/api/models.py
+++ backend/api/models.py
@@ -13,17 +13,6 @@ class Course(models.Model):
         return f"{self.subject} {self.code}"
 
 
-class Term(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="terms", default=""
-    )
-    study_term = models.CharField(max_length=4)
-    courses = models.JSONField()
-
-    def __str__(self):
-        return f"{self.study_term}"
-
-
 class UserTerms(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_terms")
     terms = models.JSONField(default=list)
@@ -31,8 +20,11 @@ class UserTerms(models.Model):
     def __str__(self):
         return f"{self.user}'s terms"
 
+
 class Degree(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_program")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_program"
+    )
     major = models.JSONField(default=list)
     minor = models.JSONField(default=list)
     option = models.JSONField(default=list)
@@ -41,10 +33,7 @@ class Degree(models.Model):
         return f"{self.user}'s program"
 
 
-
-
-
-# contains 3 JSON fields, Major, Minor and Option 
+# contains 3 JSON fields, Major, Minor and Option
 
 # class Program(models.Model):
 

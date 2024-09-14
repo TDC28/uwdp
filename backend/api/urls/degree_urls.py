12e4619edc21bacacce7d6f623eeffda23913375
LAPTOP-NL7VVFAM\assam <assamshiraz5@gmail.com>
2024-09-14 10:10:02 -0400

    modified degree url

--- backend/api/urls/degree_urls.py
+++ backend/api/urls/degree_urls.py
@@ -3,7 +3,7 @@ from api.views.degree_views import full_program
 from django.urls import path
 
 urlpatterns = [
-    path("full-program/", full_program, name="student's full program"),
+    path("", full_program, name="student's full program"),
 ]
 
 

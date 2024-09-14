030590ba1cc2b13721a69d62218937fcefa1b451
TDC28 <a06x03g07t01@protonmail.com>
2024-09-14 17:13:38 -0400

    Initializing new frontend

---
+++ frontend-old/app/page.tsx
@@ -0,0 +1,5 @@
+import Nav from "@/components/Nav";
+
+export default function Home() {
+  return <> </>;
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,19 @@
-import Nav from "@/components/Nav";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
-  return <> </>;
+  return (
+    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:test-7xl">
+        Plan your <span className="text-green-600">UW degree</span> like never
+        before
+      </h1>
+
+      <Link className={buttonVariants()} href="/dashboard">
+        Get started <ArrowRight className="ml-2 h-4 w-4" />
+      </Link>
+    </MaxWidthWrapper>
+  );
 }

5f6d6a12cca3b4d694e8ebfa7e9c79bc3ab747d2
TDC28 <a06x03g07t01@protonmail.com>
2024-09-16 09:59:22 -0400

    Navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,14 +1,32 @@
+import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "./ui/button";
 
 const Navbar = () => {
   return (
-    <nav className="sticky">
-      <MaxWidthWrapper>
+    <nav className="sticky h-14 z-30 inset-x-0 w-full border-b border-gray-200">
+      <MaxWidthWrapper className="flex flex-row items-center justify-between pt-2">
         <div>
-          <p className="text-lg font-bold">
-            <span className="text-primary">UW</span>
-            DP
-          </p>
+          <p className="text-lg font-bold">(Logo)</p>
+        </div>
+
+        <div className="flex flex-row">
+          <Link
+            href="/Log in"
+            className={buttonVariants({
+              variant: "ghost",
+            })}
+          >
+            Log in
+          </Link>
+          <Link
+            href="/dashboard"
+            className={buttonVariants({
+              variant: "ghost",
+            })}
+          >
+            Dashboard
+          </Link>
         </div>
       </MaxWidthWrapper>
     </nav>
